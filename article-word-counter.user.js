16023e51561a0470b1e3d75da76ad1de83338fdc
Lajos Santa <santa.lajos@gmail.com>
2018-05-25 18:25:44 +0200

    remove jquery from article-word-counter

--- article-word-counter.user.js
+++ article-word-counter.user.js
@@ -1,35 +1,42 @@
 // ==UserScript==
 // @name         cikk szó számláló
-// @version      1.0
+// @version      1.1
 // @description  Megszámolja hány szó van a cikkben, és kiírja hogy kb hány perc elolvasni. Jelenleg 300 szó/perc van bedrótozva. Egyelőre csak a index.hu, 444.hu, portfolio.hu oldalakon működik.
 // @author       Lajos Sánta
 // @match        https://index.hu/*
 // @match        https://444.hu/*
 // @match        https://www.portfolio.hu/*
-// @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    $(() => {
+
+    function removeByTagName(element, name) {
+        for (let child of element.getElementsByTagName(name)) {
+            child.parentElement.removeChild(child);
+        }
+    }
+
+    window.addEventListener('load', function() {
         let wordsPerMinute = 300;
         let selectors = [
             '.cikk-torzs',       // index.hu
             '#content-main',     // 444.hu
             '.smscontent',       // portfolio.hu
         ];
-        let elements = $(selectors.join(', '));
+        let elements = document.querySelectorAll(selectors.join(', '));
         if (elements.length == 1) {
             console.log('article word counter found an element!');
-            let origArticle = $(elements[0]);
-            let article = origArticle.clone();
+            let origArticle = elements[0];
+            let article = origArticle.cloneNode(true);
             // remove all unreadable tags
-            article.find('script').remove();
-            article.find('img').remove();
-            let wordCount = article.text().split(/\s+/).length;
+            removeByTagName(article, 'script');
+            removeByTagName(article, 'img');
+            let wordCount = article.textContent.split(/\s+/).length;
             let mins = Math.ceil(wordCount / wordsPerMinute);
-            //let hours = parseInt(mins / 60);
-            origArticle.prepend($(`<p>⏱ <i>${mins} perc (${wordCount} szó)</i></p>`));
+            let feedback = document.createElement('p');
+            feedback.innerHTML = `⏱ <i>${mins} perc (${wordCount} szó)</i>`;
+            origArticle.insertBefore(feedback, origArticle.childNodes[0]);
         }
-    });
+    }, true);
 })();
